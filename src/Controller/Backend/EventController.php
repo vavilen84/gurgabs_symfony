5e268fe676de4e614a5724f113ce44dcf080c3b4
Vladimir Teplov <vteplov@brightgrove.com>
2019-11-29 13:18:17 +0200

    events: fix css

--- src/Controller/Backend/EventController.php
+++ src/Controller/Backend/EventController.php
@@ -20,7 +20,7 @@ class EventController extends AbstractController
      */
     public function index(EventRepository $eventRepository): Response
     {
-        return $this->render('event/index.html.twig', [
+        return $this->render('backend/event/index.html.twig', [
             'events' => $eventRepository->findAll(),
         ]);
     }
